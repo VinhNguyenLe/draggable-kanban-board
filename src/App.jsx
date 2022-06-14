a1337b4b1d8d05ad98cd9fd36bbea19f0dd4fc97
VinhNguyenLe <levinhbn99@gmail.com>
2022-06-14 23:32:16 +0700

    final

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,13 @@
 import './App.scss';
-
+import Kanban from './components/Kanban';
 function App() {
+
   return (
-    <div className="App">
-      Vinh
+    <div style={{ padding: '50px' }} className='App'>
+      <h1 style={{ marginBottom: '20px' }}>
+        Kanban UI
+      </h1>
+      <Kanban />
     </div>
   );
 }
